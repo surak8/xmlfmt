2ea167318e8951d9da50d695aa26ffda6fe15d4d
rik cousens <rcousens@colt.com>
2019-02-25 16:10:49 -0500

    new changes.

--- Source/inference_util.cs
+++ Source/inference_util.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Text;
 using System.Xml;
 using System.Xml.Schema;
+using System.Xml.Serialization;
 using NSXmlfmt;
 
 public class driver {
@@ -48,95 +49,88 @@ public class driver {
         StringBuilder sb;
         CodeDomProvider cdp;
         CodeGeneratorOptions opts;
-        //cdp = cCodeDomProvider.GetLanguageFromExtension("cs");
+
         cdp = new Microsoft.CSharp.CSharpCodeProvider();
         opts = new CodeGeneratorOptions();
         opts.BlankLinesBetweenMembers = false;
-        opts.BracingStyle = "C";
         opts.ElseOnClosing = true;
         using (XmlReader xr = XmlReader.Create(inFile)) {
-
-
             xss = xsi.InferSchema(xr);
             foreach (XmlSchema xs in xss.Schemas()) {
                 ccu = new CodeCompileUnit();
                 ccu.Namespaces.Add(ns = new CodeNamespace());
-                iterateThroughSchema(xs,ns);
+                iterateThroughSchema(xs, ns);
                 using (StringWriter sw = new StringWriter(sb = new StringBuilder())) {
                     cdp.GenerateCodeFromCompileUnit(ccu, sw, opts);
                 }
                 Trace.WriteLine(sb.ToString());
                 sb.Length = 0;
             }
-
         }
         Trace.WriteLine("here");
         return null;
     }
 
-    static void iterateThroughSchema(XmlSchema xs,CodeNamespace ns) {
-        //Trace.WriteLine("here");
-        CodeTypeDeclaration ctd;
+    static void iterateThroughSchema(XmlSchema xs, CodeNamespace ns) {
         foreach (XmlSchemaObject anobj in xs.Items) {
-            //Trace.WriteLine("here");
-
             if (anobj is XmlSchemaElement) {
                 Trace.IndentLevel++;
-                //ns.Types.Add(ctd=)
-                processElement(anobj as XmlSchemaElement,ns);
+                processElement(anobj as XmlSchemaElement, ns, null);
                 Trace.IndentLevel--;
             } else
                 Trace.WriteLine("found: " + anobj.GetType().FullName);
         }
     }
 
-    static void processElement(XmlSchemaElement xse,CodeNamespace ns) {
-        //Trace.WriteLine("here");
-
-        //CodeTypeDeclaration
+    static void processElement(XmlSchemaElement xse, CodeNamespace ns, CodeTypeDeclaration ctdParent) {
         CodeTypeDeclaration ctd;
-        Trace.WriteLine("found: " + xse.Name + ", Type=" + xse.SchemaType+", Min="+xse.MinOccurs+", Max="+xse.MaxOccurs);
+        CodeMemberField f;
+        CodeTypeReference ctrString, ctrXmlElement;
+
+        Trace.WriteLine("found: " + xse.Name + ", Type=" + xse.SchemaType + ", Min=" + xse.MinOccurs + ", Max=" + xse.MaxOccurs);
         if (xse.SchemaType != null) {
             if (xse.SchemaType is XmlSchemaComplexType) {
-                //processComplexType(xmlSchemaElement.KC)
-                //Trace.WriteLine("here");
                 ns.Types.Add(ctd = new CodeTypeDeclaration("Class" + xse.Name));
                 Trace.IndentLevel++;
-                handleComplexType(xse.SchemaType as XmlSchemaComplexType,ns,ctd);
+                handleComplexType(xse.SchemaType as XmlSchemaComplexType, ns, ctd);
                 Trace.IndentLevel--;
             } else
                 Trace.WriteLine("unhandled: " + xse.SchemaType.GetType().FullName);
+        } else if (string.Compare(xse.SchemaTypeName.Name, "string") == 0) {
+            Trace.WriteLine("schema-type-name:" + xse.SchemaTypeName);
+            ctrString = new CodeTypeReference(typeof(string));
+            ctrXmlElement = new CodeTypeReference(typeof(XmlElementAttribute));
+            ctdParent.Members.Add(f = new CodeMemberField(ctrString, "_" + xse.Name.ToLower()));
+
+            f.CustomAttributes.Add(
+                new CodeAttributeDeclaration(
+                    new CodeTypeReference("XmlElement"),
+                    new CodeAttributeArgument(new CodePrimitiveExpression(xse.Name))));
+            f.Attributes = MemberAttributes.Public;
         } else {
-            //Trace.WriteLine("what's this");
             Trace.WriteLine("schema-type-name:" + xse.SchemaTypeName);
         }
     }
 
-    static void handleComplexType(XmlSchemaComplexType xsct,CodeNamespace ns,CodeTypeDeclaration ctdParent) {
-        CodeTypeDeclaration ctd;
-        //Trace.WriteLine("Here");
+    static void handleComplexType(XmlSchemaComplexType xsct, CodeNamespace ns, CodeTypeDeclaration ctdParent) {
         if (xsct.Particle is XmlSchemaSequence) {
             Trace.IndentLevel++;
-            handleSequence(xsct.Particle as XmlSchemaSequence);
+            handleSequence(xsct.Particle as XmlSchemaSequence, ns, ctdParent);
             Trace.IndentLevel--;
         } else
             Trace.WriteLine("unhandled: " + xsct.Particle.GetType().FullName);
     }
 
-    static void handleSequence(XmlSchemaSequence xss) {
-        //Trace.WriteLine("Here");
+    static void handleSequence(XmlSchemaSequence xss, CodeNamespace ns, CodeTypeDeclaration ctdParent) {
         Trace.WriteLine("have " + xss.GetType().FullName + " Min=" + xss.MinOccurs + ", Max=" + xss.MaxOccurs);
         foreach (XmlSchemaObject xso in xss.Items) {
-            //Trace.WriteLine("found: " + xso.GetType().FullName);
             if (xso is XmlSchemaElement) {
                 Trace.IndentLevel++;
 #warning "fix this"
-                processElement(xso as XmlSchemaElement,null);
+                processElement(xso as XmlSchemaElement, ns, ctdParent);
                 Trace.IndentLevel--;
             } else
                 Trace.WriteLine("found: " + xso.GetType().FullName);
         }
     }
-
-
 }
